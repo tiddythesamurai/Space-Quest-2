adde0c45e4dd8958b4de93cc0176658b7de3e865
Todd <knight.todd.04@gmail.com>
2018-03-06 13:32:00 -0800

    “lol”

--- Game.java
+++ Game.java
@@ -50,8 +50,9 @@ public class Game {
 		Scanner in = new Scanner(System.in);
 		String name = in.nextLine();
 		System.out.println("Nice to meet you " + name);
+		TimeUnit.SECONDS.sleep(1);
 		System.out.println("I am Personal Assistant #" + rand(0, 99999));
-		
+		TimeUnit.SECONDS.sleep(1);
 		Boolean genderBool = false;
 		System.out.print("Now I need your gender for identification purposes: ");
 		
@@ -65,12 +66,14 @@ public class Game {
 		
 			case "m":
 				
+				TimeUnit.SECONDS.sleep(1);
 				System.out.println("So, " + name + ", you are a guy.");
 				genderBool = true;
 				break;
 				
 			case "f":
 				
+				TimeUnit.SECONDS.sleep(1);;
 				System.out.println("So, " + name + ", you are a girl.");
 				genderBool = true;
 				break;
@@ -84,6 +87,29 @@ public class Game {
 		
 	}while (genderBool == false);
 	
+	System.out.print("Now its time to pick a name for your ship, Captain: ");
+	String shipName = in.nextLine();
+	Boolean shipNameBool = false;
+	do {	
+		if(shipName != "") {
+			shipNameBool = true;
+			System.out.println("\"" + shipName + ".\" That’s not a bad name.");
+			TimeUnit.SECONDS.sleep(1);
+			System.out.println("Now, your ship doesn’t exactly exist at this moment because it doen’t have a class.");
+		}else{
+			
+			System.out.println("Unfortunately, your ship name cannot be empty.");
+			
+		}
+	}while (shipNameBool == false);
+	
+	System.out.println("Don’t worry, I will walk you through the classes and stats.");
+	TimeUnit.SECONDS.sleep(1);
+	System.out.println("The first stat is health.");
+	TimeUnit.SECONDS.sleep(1);
+	System.out.println("Each class has a different amount of health, heavy has more, light has less.");
+	TimeUnit.SECONDS.sleep(2);
+	System.out.println("The next stat is shield");
 	in.close();
 		
 	}
