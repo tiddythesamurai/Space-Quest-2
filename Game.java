653e9e69d464767f983320a1268c26ee05954e5d
Todd <knight.todd.04@gmail.com>
2018-03-06 08:42:06 -0800

    “yeet”

--- Game.java
+++ Game.java
@@ -51,9 +51,40 @@ public class Game {
 		String name = in.nextLine();
 		System.out.println("Nice to meet you " + name);
 		System.out.println("I am Personal Assistant #" + rand(0, 99999));
-		in.close();
 		
+		Boolean genderBool = false;
+		System.out.print("Now I need your gender for identification purposes: ");
 		
+	do {
+		
+		String gender = in.nextLine();
+		gender.toLowerCase();
+		
+	
+		switch (gender) {
+		
+			case "m":
+				
+				System.out.println("So, " + name + ", you are a guy.");
+				genderBool = true;
+				break;
+				
+			case "f":
+				
+				System.out.println("So, " + name + ", you are a girl.");
+				genderBool = true;
+				break;
+				
+			default:
+				
+				System.out.print("Please input a valid gender: ");
+				gender = "";
+				
+		}
+		
+	}while (genderBool == false);
+	
+	in.close();
 		
 	}
 	
